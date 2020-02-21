c6c5d8ee643ac0479977896a8f36198f7e5226e6
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-02-21 12:39:13 -0500

    Create fluency.js

---
+++ fluency.js
@@ -0,0 +1,27 @@
+Using this link `https://jsonplaceholder.typicode.com/todos`, solve the following questions below. Make sure you use fetch, and
+if you would like, you can use then() methods or use async/await. Whatever is easier for you! I would recommend using
+async/await, however. Be sure to use high-order array methods for this as well!
+
+1. Create a function, `getCompleted`, that returns those that have completed tasks.
+
+2. Create a function, `getUncompleted`, that returns those that have uncompleted tasks.
+
+3. Create a function, `userIdTasks`, that takes in a user ID and returns all of their tasks, both completed and uncompleted.
+
+4. Create a function, `idTask`, that takes in an id and returns that task's title.
+
+5. Create a function, `urgentTitle`, that returns all task titles with uppercase letters.
+
+
+Now, using this link `https://jsonplaceholder.typicode.com/posts`, solve the following questions below. Same as before, use fetch!
+
+6. Create a function, `getPost`, that takes in an id and returns that post.
+
+7. Create a function, `userPosts`, that takes in a user ID and returns all the posts from that user.
+
+8. Create a function, `longestTitle`, that returns the post with the longest title.
+
+9. Create a function, `properTitle`, that returns the post's title but formatted properly. For example: "Eum Et Est Occaecati"
+(notice how each starting letter in a word is uppercase!)
+
+10. Create a function, `identify`, that returns an array with this format: "User (user's ID): (title)"
